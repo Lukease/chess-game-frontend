f7e9a6dfaeb8d6a53628f0c0d241d7564921b76b
Lukease <lukpawlowski@gmail.com>
2022-08-18 19:00:00 +0200

    add onclick and disabled attr to buttons: game__navigation--start, game__navigation--editor. create constructor in class GameNavigation. create new function handleClick() in class GameNavigation. create new class: game__whose-turn, game__color. add in function handleClick color white.

--- src/Arena.tsx
+++ src/Arena.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Arena.css'
-import {Field, FieldNumber, Letter} from './Arena_utils'
+import { Field, FieldNumber, Letter } from './Arena_utils'
 import {
     addNewFigure,
     getFigure,
@@ -11,17 +11,57 @@ import {
 } from './Arena_utils/new-figure'
 
 class GameNavigation extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props)
+
+        this.state = {isToggleOn: false}
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState((prevState: { isToggleOn: any }) => ({
+            isToggleOn: !prevState.isToggleOn
+        }))
+
+        const addFigureColumn: NodeListOf<Element> = document.querySelectorAll('.game__add-figure')
+
+        addFigureColumn.forEach(column => {
+            if (!column.classList.contains('game__add-figure--hidden')) {
+                column.classList.add('game__add-figure--hidden')
+            }
+        })
+
+        document.getElementById('color')!.innerHTML = 'white'
+
+        alert('let s start! White starts.')
+    }
+
     render() {
         return (
             <div className={'game__navigation'}>
-                <button className={'game__navigation--start'}>
+                <button
+                    className={'game__navigation--start'}
+                    onClick={this.handleClick}
+                    disabled={!!this.state.isToggleOn}
+                >
                     Start Game
                 </button>
                 <button
                     className={'game__navigation--editor'}
+                    disabled={!!this.state.isToggleOn}
                 >
                     Position Editor
                 </button>
+                <div
+                    className={'game__whose-turn'}
+                >
+                    Whose turn:
+                </div>
+                <div
+                    id={'color'}
+                    className={'game__color'}
+                >
+                </div>
             </div>
         )
     }
